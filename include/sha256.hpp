2995079b6cab40024a21f26b9402abd15e51f820
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2022-05-15 12:36:54 -0500

    feat(sha256): Implement for integers

--- include/sha256.hpp
+++ include/sha256.hpp
@@ -18,10 +18,36 @@ auto right_rotate(Integer n, unsigned rotation) -> Integer
     return n >> rotation | n << (std::numeric_limits<Integer>::digits - rotation);
 }
 
-template<typename T>
-auto sha256(T) -> std::array<uint32_t, 8>;
-
 auto sha256(std::vector<bool> bytes) -> std::array<uint32_t, 8>;
+
+template<
+    typename Integer,
+    std::enable_if_t<std::numeric_limits<Integer>::is_integer, bool> = true>
+auto sha256(Integer n, bool strip_leading_zeroes = true) -> std::array<uint32_t, 8>
+{
+    std::vector<bool> integer_in_bits;
+    int n_digits = std::numeric_limits<Integer>::digits;
+
+    if (strip_leading_zeroes)
+    {
+        while (n_digits > 0)
+        {
+            if ((n >> (n_digits - 1)) & 1)
+            {
+                break;
+            }
+            --n_digits;
+        }
+    }
+
+    while (n_digits > 0)
+    {
+        integer_in_bits.push_back(n >> (n_digits - 1) & 1);
+        --n_digits;
+    }
+
+    return sha256(integer_in_bits);
+}
 
 auto to_hex_string(uint32_t n) -> std::string;
 auto to_hex_string(std::bitset<4> n) -> std::string;
