27c608c987962c9f7ab4c67e30f55c6252d79aaa
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2022-05-15 10:53:37 -0500

    test(sha256): Add tests

---
+++ tests/test_sha256.cpp
@@ -0,0 +1,49 @@
+#include "catch.hpp"
+#include "sha256.hpp"
+
+TEST_CASE("to hex", "[sha256]")
+{
+    CHECK(to_hex_string(1) == "00000001");
+    CHECK(to_hex_string(1 << 4) == "00000010");
+    CHECK(to_hex_string(1 << 8) == "00000100");
+    CHECK(to_hex_string(1 << 12) == "00001000");
+    CHECK(to_hex_string(1 << 16) == "00010000");
+    CHECK(to_hex_string(1 << 20) == "00100000");
+    CHECK(to_hex_string(1 << 24) == "01000000");
+    CHECK(to_hex_string(1 << 28) == "10000000");
+
+    CHECK(to_hex_string(10) == "0000000A");
+    CHECK(to_hex_string(15) == "0000000F");
+
+    std::array<uint32_t, 8> v1 = {
+        0xe3b0c442,
+        0x98fc1c14,
+        0x9afbf4c8,
+        0x996fb924,
+        0x27ae41e4,
+        0x649b934c,
+        0xa495991b,
+        0x7852b855};
+
+    CHECK(
+        to_hex_string(v1.begin(), v1.end())
+        == "E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855");
+}
+
+TEST_CASE("sha256", "[sha256]")
+{
+    CHECK(
+        sha256_hex(std::deque<bool>())
+        == "E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855");
+
+    CHECK(
+        // == "hello"
+        sha256_hex(std::deque<bool>{0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0,
+                                    1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1})
+        == "2CF24DBA5FB0A30E26E83B2AC5B9E29E1B161E5C1FA7425E73043362938B9824");
+
+    CHECK(
+        // == 128
+        sha256_hex(std::deque<bool>{1, 0, 0, 0, 0, 0, 0, 0})
+        == "76BE8B528D0075F7AAE98D6FA57A6D3C83AE480A8469E668D7B0AF968995AC71");
+}
