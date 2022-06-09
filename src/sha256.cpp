e1e2801ecf48f237d484ef44db9abe01f9240de9
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2022-06-09 18:35:43 -0500

    fix(sha256): `uint8_t*` not working properly

--- src/sha256.cpp
+++ src/sha256.cpp
@@ -126,7 +126,7 @@ auto sha256(std::vector<bool> bits) -> std::array<uint32_t, 8>
     return {h0, h1, h2, h3, h4, h5, h6, h7};
 }
 
-auto sha256(uint8_t const* octets, size_t n_octets) -> std::array<uint32_t, 8>
+auto sha256(uint8_t const* octets, uint64_t n_octets) -> std::array<uint32_t, 8>
 {
     uint32_t h0 = 0x6a09e667;
     uint32_t h1 = 0xbb67ae85;
@@ -137,7 +137,7 @@ auto sha256(uint8_t const* octets, size_t n_octets) -> std::array<uint32_t, 8>
     uint32_t h6 = 0x1f83d9ab;
     uint32_t h7 = 0x5be0cd19;
 
-    static constexpr std::array<uint32_t, 64>  round_constants = {
+    static constexpr std::array<uint32_t, 64> round_constants = {
         0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4,
         0xab1c5ed5, 0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe,
         0x9bdc06a7, 0xc19bf174, 0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f,
@@ -148,8 +148,6 @@ auto sha256(uint8_t const* octets, size_t n_octets) -> std::array<uint32_t, 8>
         0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
         0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7,
         0xc67178f2};
-
-    size_t remaining_octets = n_octets;
 
     // 512 bits are processed each time around
     auto process_512 = [&](auto const& w_given) {
@@ -216,80 +214,60 @@ auto sha256(uint8_t const* octets, size_t n_octets) -> std::array<uint32_t, 8>
         h7 += h;
     };
 
-    while (remaining_octets > 0)
-    {
-        if (512 <= remaining_octets * 8)
+    size_t message_block_size = [=]() {
+        size_t size_without_padding = n_octets * 8 + 8 + 64;
+        size_t m512 = size_without_padding % 512;
+        if (m512 == 0)
         {
-            // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
-            auto w_given = reinterpret_cast<uint32_t const*>(octets);
-            process_512([&](size_t index) { return w_given[index]; });
-
-            remaining_octets -= 512 / 8;
-        }
-        // We can fit the remaining octets and the size in the same 512 block
-        else if (remaining_octets * 8 + 64 <= 512)
-        {
-            // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
-            auto octs = reinterpret_cast<uint32_t const*>(octets);
-
-            process_512([&](size_t index) {
-                if (index < remaining_octets / 4)
-                {
-                    return octs[index];
-                }
-                else if (index == remaining_octets / 4)
-                {
-                    return uint32_t(0x80000000);
-                }
-                else if (index <= 13)
-                {
-                    return uint32_t{0};
-                }
-                else if (index == 14)
-                {
-                    return static_cast<uint32_t>((n_octets * 8) >> 32);
-                }
-                else
-                {
-                    return static_cast<uint32_t>(n_octets * 8);
-                }
-            });
-
-            break;
+            return size_without_padding;
         }
         else
         {
-            // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
-            auto octs = reinterpret_cast<uint32_t const*>(octets);
-
-            process_512([&](size_t index) {
-                if (index < remaining_octets / (32 / 8))
-                {
-                    return octs[index];
-                }
-                else
-                {
-                    return uint32_t{0};
-                }
-            });
-
-            process_512([&](size_t index) {
-                if (index <= 13)
-                {
-                    return uint32_t{0};
-                }
-                else if (index == 14)
-                {
-                    return static_cast<uint32_t>((n_octets * 8) >> 32);
-                }
-                else
-                {
-                    return static_cast<uint32_t>(n_octets * 8);
-                }
-            });
-
-            break;
+            return size_without_padding + 512 - m512;
         }
+    }();
+    assert(message_block_size % 512 == 0);
+
+    bool should_break = false;
+    auto get_octet = [&](size_t index) {
+        if (index < n_octets)
+        {
+            return octets[index];
+        }
+        else if (index == n_octets)
+        {
+            return uint8_t{0x80};
+        }
+        // The size is 64 bits long = 8 octets
+        else if ((message_block_size / 8 - 8) <= index)
+        {
+            size_t size_index = index - (message_block_size / 8 - 8);
+            if (size_index == 7)
+            {
+                should_break = true;
+            }
+            return static_cast<uint8_t>((n_octets * 8) >> (8 * (7 - size_index)));
+        }
+        else
+        {
+            return uint8_t(0);
+        }
+    };
+
+    size_t octets_read = 0;
+    while (!should_break)
+    {
+        auto get32 = [&](size_t index) {
+            uint32_t octet1 = get_octet(index * 4 + octets_read);
+            uint32_t octet2 = get_octet(index * 4 + octets_read + 1);
+            uint32_t octet3 = get_octet(index * 4 + octets_read + 2);
+            uint32_t octet4 = get_octet(index * 4 + octets_read + 3);
+
+            return (octet1 << 24) | (octet2 << 16) | (octet3 << 8) | octet4;
+        };
+
+        process_512(get32);
+        octets_read += 512 / 8;
     }
 
     return {h0, h1, h2, h3, h4, h5, h6, h7};
