0f653efc19b306bdb9dc6c8c571c2425931a1676
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2022-06-10 00:43:28 -0500

    build(nix/shell): Pin nixpkgs and fix missing headers

--- shell.nix
+++ shell.nix
@@ -1,11 +1,16 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import (builtins.fetchTarball {
+  name = "nixos-clangd-working";
+  url =
+    "https://github.com/nixos/nixpkgs/archive/9e49886b3d83d18ca59f66ec7b388315fa3d2e60.tar.gz";
+  sha256 = "0bkdm4wf5vlg6mgv6r6672imx84axi5s159jd1f90gj5da2mzmfm";
+}) { } }:
 
 with pkgs;
 
-mkShell {
-  buildInputs = [ ];
+mkShell rec {
+  buildInputs = [ llvmPackages_latest.libcxx ];
 
-  nativeBuildInputs = [
+  packages = [
     clang_14
     gcc
 
@@ -21,4 +26,6 @@ mkShell {
     ninja
     pkg-config
   ];
+
+  CPATH = lib.makeSearchPathOutput "dev" "include" buildInputs;
 }
