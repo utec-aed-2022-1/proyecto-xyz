af4e7159e38b25e29774b4af39cd3c4d942f7011
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2022-06-09 21:43:13 -0500

    test(sha256): Add tests for strings

--- tests/test_sha256.cpp
+++ tests/test_sha256.cpp
@@ -88,3 +88,17 @@ TEST_CASE("sha256 integers", "[sha256]")
         to_hex_string(sha256(uint64_t{0xFF}))
         == "B8862208A16D307A1E2B3A5DF2B44A63126BBA7CD20C985C436135AD88D2C735");
 }
+
+TEST_CASE("sha256 strings", "[sha256]")
+{
+    using namespace std::string_literals;
+
+    CHECK(
+        sha256_hex("hello"s)
+        == "2CF24DBA5FB0A30E26E83B2AC5B9E29E1B161E5C1FA7425E73043362938B9824");
+
+    CHECK(
+        sha256_hex(
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam volutpat eleifend sem, in facilisis massa fermentum quis. Etiam efficitur nulla ac nulla tempus lobortis. Duis mattis vulputate libero quis interdum. Proin eleifend dignissim mi ut pretium. Nunc congue congue ex, vel pretium leo faucibus non. Integer accumsan quam sit amet velit tristique, et vestibulum massa molestie. Nulla facilisi. Aliquam eleifend augue eget viverra bibendum. Fusce ornare sed lectus eget egestas. Duis porttitor urna id lacinia dictum. Ut sed lectus efficitur, hendrerit metus in, venenatis purus.  Aliquam justo erat, fermentum id placerat non, pharetra auctor purus. Vestibulum pharetra tincidunt nulla eu iaculis. In sollicitudin arcu eu suscipit luctus. Vivamus aliquet placerat ipsum, sed sagittis est maximus nec. Proin eu ante augue. Pellentesque volutpat vehicula viverra. Cras dui dui, consectetur nec erat sed, condimentum lacinia nunc. Phasellus vehicula enim vitae mi lobortis congue. Duis eu blandit mauris.  Nulla facilisi. Nunc at auctor sem, quis pulvinar turpis. Donec in lacinia tortor, et vestibulum eros. Vestibulum tortor tellus, eleifend quis pulvinar eu, fermentum varius magna. In commodo odio orci, vestibulum molestie odio tristique at. Nam dignissim purus in purus pulvinar, eu malesuada velit ullamcorper. Nullam nec elit sed diam accumsan congue ut sed ipsum. Fusce auctor turpis ac nisl semper, vitae sollicitudin ligula aliquet. Nam eget odio sed libero vehicula faucibus. Maecenas malesuada mattis justo, nec rhoncus ex ullamcorper sit amet. Fusce dignissim a ligula ut facilisis.  Nam ac sapien a sem tempor faucibus eget id massa. Curabitur a ante ex. Phasellus venenatis mollis eros quis fermentum. Vivamus a mattis diam, ut sodales ligula. Nulla sagittis, arcu sed fermentum molestie, nisl urna commodo ex, eget eleifend mauris felis eu turpis. Duis nec nulla sit amet diam congue accumsan. Vestibulum fringilla urna vitae ornare luctus. Donec eget odio efficitur, volutpat nisi sit amet, convallis lorem. Pellentesque erat lorem, placerat vestibulum ex nec, viverra consequat mi. Mauris congue condimentum lacus, sit amet congue diam pretium ut.  Mauris feugiat posuere fringilla. Cras rutrum ultricies arcu, vitae elementum sem varius quis. Integer et enim odio. Praesent egestas pulvinar neque nec ultrices. Etiam ultricies nisi vitae tempor porttitor. Vivamus aliquam finibus leo imperdiet tristique. Ut non magna suscipit felis vestibulum laoreet. Phasellus eu turpis dui. Morbi ultricies risus quis dolor placerat, id consequat orci vulputate. Cras varius, turpis vitae tempus viverra, nunc mauris pharetra ex, eu efficitur tortor justo vitae purus."s)
+        == "4526444E57E84CA4D3EB63DE186F3D86CC19911550F3DB26106518FD4274E3BC");
+}
