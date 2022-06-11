62f6efc558c93fb4d62ff9f29cccffec99063efa
gracenik <grace@destacame.cl>
2022-06-11 13:41:52 -0500

    modified(main): format and block struct test

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,26 @@
 #include <array>
 #include <iostream>
 
-#include "sha256.hpp"
-#include "block.hpp"
 #include "../include/json.hpp"
+#include "block.hpp"
+#include "sha256.hpp"
 
 using json = nlohmann::json;
 using namespace std;
 
-auto main() -> int
-{
-    block blck(1, "Hello World");
+auto main() -> int {
+  block blck(1, "Hello World");
 
-    // cout << blck.getHash() << endl;
-    // cout << blck.getPrevHash() << endl;
+  // cout << blck.getHash() << endl;
+  // cout << blck.getPrevHash() << endl;
 
-    cout << "Hash inicial (id y data): " << endl;
-    cout << blck.calculateHash() << endl;
+  cout << "Hash inicial (id y data): " << endl;
+  cout << blck.calculateHash() << endl;
 
-    cout << "Mina el bloque: " << endl;
-    json jsxn = blck.mineBlock(3);
+  cout << "Mina el bloque: " << endl;
+  json jsxn = blck.mineBlock(3);
 
-    blck.saveInJson(jsxn);
+  blck.saveInJson(jsxn);
 
-    return 0;
+  return 0;
 }
