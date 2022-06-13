48e45fa81dec6ea14245dc0d75d668d7a3b532ce
Elmau1618 <mauricio.nieto@utec.edu.pe>
2022-06-12 20:38:42 -0500

    serialize and deserialize in respective directories

--- include/blockchain.hpp
+++ include/blockchain.hpp
@@ -21,18 +21,24 @@ class Blockchain {
   blocks_t bc;
   uint32_t difficulty = 3;
   std::string jsonfile = "mainjson.json";
+  bool valid_bc = false;
+  bool destructionserialize = false;
+
+  bool deserialize_private(const std::string& filename);
+  bool serialize_private(std::string filename);
 
  public:
   Blockchain() = default;
-  explicit Blockchain(bool deserialize);
-  explicit Blockchain(const std::string& filename);
+  explicit Blockchain(bool deserialize, bool deserialize_);
+  Blockchain(std::string filename);
 
   auto getBlocks() -> blocks_t const&;
 
-  bool serialize(const std::string& filename = "mainjson.json");
-  bool deserialize(const std::string& filename = "mainjson.json");
+  bool serialize(std::string filename);
+  bool deserialize(const std::string& filename);
   bool push(TDATA data);
   bool push(json data);
+  void setserializedestruction(bool destructionserialize);
   Block front();
   Block end();
   int size();
