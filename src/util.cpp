9c0dd63997d8d98734cc24f1f4159b7f3f1a3541
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2022-06-15 00:07:46 -0500

    feat(util): Create parent directories when writing JSON

--- src/util.cpp
+++ src/util.cpp
@@ -1,5 +1,6 @@
 #include "util.hpp"
 
+#include <filesystem>
 #include <fstream>
 
 #include "json.hpp"
@@ -7,6 +8,10 @@
 using json = nlohmann::json;
 
 void jsonToFile(std::string const& filepath, json const& j) {
+  namespace fs = std::filesystem;
+  fs::path p(filepath);
+  fs::create_directories(p.parent_path());
   std::ofstream fp(filepath);
+  assert(fp.is_open());
   fp << j;
 }
