aac899ba2c372c868193138b09fcc95558a6b4bf
Elmau1618 <mauricio.nieto@utec.edu.pe>
2022-06-15 10:43:41 -0500

    feat(main): Add push examples

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,26 @@
 #include <array>
 #include <iostream>
 
-#include "json.hpp"
 #include "blockchain.hpp"
+#include "json.hpp"
 
 using json = nlohmann::json;
 using namespace std;
 
-auto main() -> int {
-  // from console
-  Block blck(1, "Hello World");
+auto main() -> int { /*
+   // from console
+   Block blck(1, "Hello World");
 
-  // cout << blck.getHash() << endl;
-  // cout << blck.getPrevHash() << endl;
+   // cout << blck.getHash() << endl;
+   // cout << blck.getPrevHash() << endl;
 
-  cout << "Hash inicial (id y data): " << endl;
-  cout << blck.calculateHash() << endl;
+   cout << "Hash inicial (id y data): " << endl;
+   cout << blck.calculateHash() << endl;
 
-  cout << "Mina el bloque: " << endl;
-  json jsxn = blck.mineBlock(3);
+   cout << "Mina el bloque: " << endl;
+   json jsxn = blck.mineBlock(3);
 
-  blck.saveInJson("aa/file.json");
+  blck.saveInJson("aa/file.json");*/
 
   // from json file (falta implementar cuando hay más de 1 registro)
   // Block blck("blocksInput/1.json");
@@ -30,9 +30,16 @@ auto main() -> int {
   // json jsxn = blck.mineBlock(3);
 
   // blck.saveInJson(jsxn);
-
   Blockchain A("mainjson.json");
   A.setserializedestruction(true);
+  A.push("camila-prestamo: 50$");
+  A.push("jefferson-prestamo: 10000$");
+  A.push("maria-prestamo: 1240$");
+  A.push("1.json", true);
+  A.edit(3, "itsme Mario aksdalda");
   A.serialize("mainjson.json");
+  A.deserialize("mainjson.json");
+  cout << "Size: " << A.size() << endl;
+  cout << "Find: " << A.find(1) << endl;
   return 0;
 }
