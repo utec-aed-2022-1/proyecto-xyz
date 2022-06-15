d5f00396d7d509f8bdbdeaa2b416f8de660c143f
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2022-06-15 11:15:38 -0500

    refactor(blockchain): Remove serialize code

--- include/blockchain.hpp
+++ include/blockchain.hpp
@@ -24,30 +24,26 @@ class Blockchain {
   bool valid_bc = false;
   bool destructionserialize = false;
 
-  bool deserialize_private(std::string filename);
-  bool serialize_private(std::string filename);
   void push(json data, size_t diferenciador);
 
  public:
   Blockchain() = default;
-  explicit Blockchain(bool deserialize, bool deserialize_);
-  Blockchain(std::string filename);
 
   auto getBlocks() -> blocks_t const&;
 
-  bool serialize(std::string filename);
-  bool deserialize(std::string filename);
   void push(TDATA data);
   void push(std::string filename, bool isfile);
-  void setserializedestruction(bool destructionserialize_);
+
   Block front();
   Block end();
+
   size_t size();
+
   TDATA find(size_t position);
   bool edit(size_t position, TDATA data);
   void clear();
 
-  ~Blockchain();
+  ~Blockchain() = default;
 };
 
 void to_json(nlohmann::json& j, Blockchain const& blocks);
--- src/blockchain.cpp
+++ src/blockchain.cpp
@@ -17,144 +17,7 @@ auto blockchainFromFile(std::string const& filepath) -> Blockchain {
   return j.get<Blockchain>();
 }
 
-Blockchain::Blockchain(bool deserialize, bool deserialize_) {
-  if (deserialize && deserialize_) {
-    if (!this->deserialize_private(jsonfile)) {
-      cout << "Deserialize ha fallado" << endl;  // TODO: rethink
-      this->valid_bc = false;
-    } else
-      this->valid_bc = true;
-  }
-}
-
-Blockchain::Blockchain(std::string filename) {
-  if (!this->deserialize(filename)) {
-    cout << "Deserialize ha fallado" << endl;  // TODO: found a better way
-    this->valid_bc = false;
-  } else {
-    this->valid_bc = true;
-  }
-}
-
 auto Blockchain::getBlocks() -> blocks_t const& { return this->bc; }
-
-bool Blockchain::serialize_private(std::string filename) {
-  std::ofstream file(filename, std::ofstream::trunc);
-
-  if (file.fail()) {
-    // File does not exist code here
-    cout << "This file does not exist" << endl;
-    return false;
-  }
-
-  file << "{\n  \"data\": [\n";
-  unsigned long long number = 0;
-  for (auto& x : this->bc) {
-    json jxsn;
-    jxsn["id"] = x.id;
-    jxsn["data"] = x.data;
-    jxsn["prev"] = x.prevHash;
-    jxsn["nonce"] = x.nonce;
-    jxsn["hash"] = x.hash;
-
-    std::string str = jxsn.dump(4);
-    file << str;
-    if (number != this->bc.size() - 1) file << ",\n";
-
-    number++;
-  }
-  file << "\n  ]\n}";
-
-  file.close();
-  return true;
-}
-
-bool Blockchain::serialize(std::string filename) {
-  if (!this->valid_bc) {
-    filename = "invalid" + filename;
-    cout << "This blockchain is not valid to serialize,"
-            " alternative serialize made in "
-         << filename << endl;
-  }
-
-  filename = "blocksRegister/" + filename;
-  return serialize_private(filename);
-}
-
-bool Blockchain::deserialize_private(std::string filename) {
-  // read json main file
-  std::ifstream file(filename, std::ifstream::in);
-
-  if (file.fail()) {
-    // File does not exist code here
-    cout << "This file does not exist" << endl;
-    return false;
-  }
-
-  // deserialize
-  char c = file.get();
-  std::string to_json;
-  while (file.good()) {  // TODO: No funciona con el operador sobrecargado <<,
-    // puede no ser la forma mas eficiente
-    to_json += c;
-    c = file.get();
-  }
-  json j = json::parse(to_json);
-
-  for (size_t i = 0; i < j["data"].size(); i++) {
-    Block blck(j["data"][i]["id"], j["data"][i]["data"], j["data"][i]["nonce"],
-               j["data"][i]["prev"], j["data"][i]["hash"]);
-
-    this->bc.push_back(blck);
-  }
-
-  // verificar si el archivo leido es valido
-  bool flag = false;
-  for (size_t i = 0; i < this->bc.size(); i++) {
-    std::string temp_hash = this->bc[i].hash;
-    uint64_t temp_nonce = this->bc[i].nonce;
-    std::string temp_prev;
-    if (i == 0)
-      temp_prev = hashZeros;
-    else
-      temp_prev = this->bc[i].prevHash;
-    uint64_t temp_id = i + 1;
-
-    this->bc[i].nonce = nonceDefaultVal;
-    this->bc[i].mineBlock(this->difficulty);
-
-    if (this->bc[i].hash != temp_hash || this->bc[i].nonce != temp_nonce ||
-        this->bc[i].prevHash != temp_prev || this->bc[i].id != temp_id) {
-      cout << "Mineblock found different values of hash or nonce or prev or id "
-              "in ";
-      cout << i << " block." << endl;
-      this->bc[i].nonce = temp_nonce;
-      this->bc[i].hash = temp_hash;
-      flag = true;
-    } else {
-      this->bc[i].nonce = temp_nonce;
-    }
-  }
-  file.close();
-  if (flag) return false;
-  return true;
-}
-
-bool Blockchain::deserialize(std::string filename) {
-  if(!this->bc.empty()) this->bc.clear();
-
-  if (!this->bc.empty()) this->bc.clear();
-
-  filename = "./blocksinput/" + filename;
-  if (filename == this->jsonfile) {
-    cout << "This is an invalid name, try again" << endl;
-    return false;
-  } else {
-    bool boolean = this->deserialize_private(filename);
-    this->valid_bc = boolean;
-    return boolean;
-  }
-}
 
 void Blockchain::push(TDATA data) {
   json j;
@@ -204,10 +67,6 @@ Block Blockchain::front() { return this->bc.front(); }
 
 Block Blockchain::end() { return this->bc.back(); }
 
-void Blockchain::setserializedestruction(bool destructionserialize_) {
-  this->destructionserialize = destructionserialize_;
-}
-
 size_t Blockchain::size() { return this->bc.size(); }
 
 TDATA Blockchain::find(size_t position) {
@@ -232,15 +91,3 @@ bool Blockchain::edit(size_t position, TDATA data) {
 }
 
 void Blockchain::clear() { this->bc.clear(); }
-
-Blockchain::~Blockchain() {
-  if (this->destructionserialize) {
-    if (!this->valid_bc)
-      cout << "This blockchain is not valid to serialize"
-              " when destructor is called";
-    else
-      serialize_private(this->jsonfile);
-  }
-
-  this->bc.clear();
-}
