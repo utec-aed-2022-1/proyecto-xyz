a4ddb78053bd38901ebffe0dedf7470764dccad3
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2022-06-18 12:12:40 -0500

    feat(util): Add `overload`

--- include/util.hpp
+++ include/util.hpp
@@ -2,4 +2,12 @@
 
 #include "json.hpp"
 
+// From: https://www.cppstories.com/2019/02/2lines3featuresoverload.html/
+template <class... Ts>
+struct overload : Ts... {
+  using Ts::operator()...;
+};
+template <class... Ts>
+overload(Ts...) -> overload<Ts...>;
+
 void jsonToFile(std::string const& filepath, nlohmann::json const& j);
