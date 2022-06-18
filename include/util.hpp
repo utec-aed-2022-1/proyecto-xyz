cff2a32aac30ce21cda9fcb3f91ba23b03cc812e
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2022-06-18 17:46:26 -0500

    feat(util): Add `getTFromJsonFile` to get an object from a JSON file

--- include/util.hpp
+++ include/util.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <fstream>
+
 #include "json.hpp"
 
 // From: https://www.cppstories.com/2019/02/2lines3featuresoverload.html/
@@ -9,5 +11,13 @@ struct overload : Ts... {
 };
 template <class... Ts>
 overload(Ts...) -> overload<Ts...>;
+
+template <typename T>
+auto getTFromJsonFile(std::string const& filepath) -> T {
+  std::ifstream fp{filepath};
+  nlohmann::json j;
+  fp >> j;
+  return j.get<T>();
+}
 
 void jsonToFile(std::string const& filepath, nlohmann::json const& j);
