87ef8a3dcddc18999740d568d6878193817d52b2
gracenik <grace@destacame.cl>
2022-06-29 22:59:03 -0500

    post front

--- xyz_front/src/utils/loginApi.js
+++ xyz_front/src/utils/loginApi.js
@@ -1,11 +1,33 @@
-export const userLogin = async ({ email, password }) => {
+const axios = require('axios')
+
+const url = 'http://localhost:8000'
+
+export const userLogin = async ({ dni, password }) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (email === '12345678' && password === 'password') {
-        resolve();
+      if (dni === '12345678' && password === 'password') {
+        resolve()
       } else {
-        reject();
+        reject()
       }
-    }, 3000);
-  });
-};
+    }, 3000)
+  })
+}
+
+export const userRegister = async ({ dni, name, password }) => {
+  console.log('userRegister')
+  axios
+    .post(`${url}/user`, {
+      dni: dni,
+      name: name,
+      password: password
+    })
+    .then(function (response) {
+      console.log(response)
+      return response
+    })
+    .catch(function (error) {
+      console.log(error)
+      return error
+    })
+}
