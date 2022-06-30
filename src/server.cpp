dab0634040976a58b5d475471a9f3ea4d5a1ad70
gracenik <grace@destacame.cl>
2022-06-30 02:19:13 -0500

    fixed server

--- src/server.cpp
+++ src/server.cpp
@@ -1,4 +1,6 @@
 #include <string>
+#include <stdio.h>
+#include <cassert>
 
 #include "bank.hpp"
 #include "bank_operations.hpp"
@@ -21,25 +23,32 @@ auto main() -> int {
       BankTransfer{"id_user 1", 111, "date 1", "id_sender 1", "id_receiver 1"});
   bank.pushOperation(BankWithdrawal{"id_user 2", 222, "date 2", "id_client 2"});
 
-  std::cerr << json{bank.getUsers()} << "\n";
+  //std::cerr << json{bank.getUsers()} << "\n";
 
   svr.set_pre_routing_handler([](const auto& req, auto& res) {
     res.set_header("Access-Control-Allow-Headers", "*");
     res.set_header("Access-Control-Allow-Origin", "*");
-    return Server::HandlerResponse::Handled;
+    if(req.method == "OPTIONS") {
+      return Server::HandlerResponse::Handled;
+    }
+    return Server::HandlerResponse::Unhandled;
+  });
+
+  svr.set_post_routing_handler([](const auto& req, auto& res) {
+    cout << "[" << req.method << "] " << req.path << " " << res.body << endl;
   });
 
   svr.Get("/users", [&bank](const Request& /*req*/, Response& res) {
-    res.set_content(json{bank.getUsers()}.dump(), "text/json");
+    res.set_content(json{bank.getUsers()}.dump(), "application/json");
   });
 
   svr.Get(R"(/user/(\d+))", [&](const Request& req, Response& res) {
     std::string const& id = req.matches[1];
-    res.set_content(json{bank.getUser(id)}.dump(), "text/json");
+    res.set_content(json{bank.getUser(id)}.dump(), "application/json");
   });
 
   svr.Post("/user", [&](const Request& /*req*/, Response& res,
-                         const ContentReader& content_reader) {
+                        const ContentReader& content_reader) {
     content_reader([&](const char* data, size_t /*data_length*/) {
       bank.addUser(json::parse(data).get<User>());
       return true;
@@ -47,7 +56,7 @@ auto main() -> int {
   });
 
   svr.Get("/operations", [&bank](const Request& /*req*/, Response& res) {
-    res.set_content(json(bank.getOperations()).dump(), "text/json");
+    res.set_content(json(bank.getOperations()).dump(), "application/json");
   });
 
   svr.Post("/operations", [&](const Request& /*req*/, Response& res,
@@ -59,5 +68,6 @@ auto main() -> int {
   });
 
   size_t const port = 8000;
+  cout << "Listening on port " << port << endl;
   svr.listen("localhost", port);
 }
