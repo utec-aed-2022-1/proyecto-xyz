2edc093ff023c8499c49508dfb19e370847b6a1f
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2022-06-30 00:17:21 -0500

    feat(server): Allow cross origin requests

--- src/server.cpp
+++ src/server.cpp
@@ -24,16 +24,22 @@ auto main() -> int {
   std::cerr << json{bank.getUsers()} << "\n";
 
   svr.Get("/users", [&bank](const Request& /*req*/, Response& res) {
+    res.set_header("Access-Control-Allow-Origin", "*");
+
     res.set_content(json{bank.getUsers()}.dump(), "text/json");
   });
 
   svr.Get(R"(/users/(\d+))", [&](const Request& req, Response& res) {
+    res.set_header("Access-Control-Allow-Origin", "*");
+
     std::string const& id = req.matches[1];
     res.set_content(json{bank.getUser(id)}.dump(), "text/json");
   });
 
-  svr.Post("/users", [&](const Request& /*req*/, Response& /*res*/,
+  svr.Post("/users", [&](const Request& /*req*/, Response& res,
                          const ContentReader& content_reader) {
+    res.set_header("Access-Control-Allow-Origin", "*");
+
     content_reader([&](const char* data, size_t /*data_length*/) {
       bank.addUser(json::parse(data).get<User>());
       return true;
@@ -41,11 +47,15 @@ auto main() -> int {
   });
 
   svr.Get("/operations", [&bank](const Request& /*req*/, Response& res) {
+    res.set_header("Access-Control-Allow-Origin", "*");
+
     res.set_content(json(bank.getOperations()).dump(), "text/json");
   });
 
-  svr.Post("/operations", [&](const Request& /*req*/, Response& /*res*/,
+  svr.Post("/operations", [&](const Request& /*req*/, Response& res,
                               const ContentReader& content_reader) {
+    res.set_header("Access-Control-Allow-Origin", "*");
+
     content_reader([&](const char* data, size_t /*data_length*/) {
       bank.pushOperation(json::parse(data).get<BankOperation>());
       return true;
