1d0a4b296e6887e2dfead111cff902a7abcf3229
gracenik <grace@destacame.cl>
2022-07-03 00:22:58 -0500

    login and sign up

--- xyz_front/components/LoginForm.js
+++ xyz_front/components/LoginForm.js
@@ -1,5 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
 import React, { useState, useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import { useRouter } from 'next/router'
 
 import {
   FormControl,
@@ -14,73 +16,87 @@ import {
   Heading
 } from '@chakra-ui/react'
 
-import { userLogin, userRegister } from '../src/utils/loginApi'
-import ErrorMessage from './ErrorMessage'
-
-import axios from 'axios'
+const axios = require('axios')
 
 const LoginForm = () => {
-  const [dni, setDni] = useState('')
-  const [name, setName] = useState('')
-  const [password, setPassword] = useState('')
+  const [data, setData] = useState('')
   const [error, setError] = useState('')
   const [isLoading, setIsLoading] = useState(false)
   const [registerForm, setRegisterForm] = useState(0)
+  const router = useRouter()
 
-  const [user, getUser] = useState('')
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm()
 
   const url = 'http://localhost:8000'
 
   useEffect(() => {
-    getUsers()
+    // getUsers()
   }, [])
-
-  const getUsers = () => {
-    axios
-      .get(`${url}/users`)
-      .then((res) => {
-        const allUsers = res.data[0]
-        console.log('allUsers', allUsers)
-        getUser(allUsers)
-      })
-      .catch((err) => console.log(err))
-  }
 
   function changeForm() {
     setRegisterForm(!registerForm)
-    console.log('registerForm', registerForm)
   }
 
-  const handleSubmit = async (event) => {
-    console.log('registerForm', registerForm)
-    event.preventDefault()
-    setIsLoading(true)
-    // Login
+  const userRegister = async ({ dni, name, password }) => {
+    console.log('userRegister', dni, name, password)
+    try {
+      const response = axios.post(`${url}/user`, {
+        dni: dni,
+        name: name,
+        password: password
+      })
+      setData('')
+      return response
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  const userLogin = async ({ dni, password }) => {
+    console.log('userLogin', dni, password)
+    try {
+      const response = axios.get(`${url}/user/${dni}`, {
+        dni: dni,
+        password: password
+      })
+      setData('')
+      return response
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  const onSubmit = async (data) => {
+    setData(data)
     if (!registerForm) {
       try {
-        await userLogin({ dni, password })
+        const result = await userLogin(data)
         setIsLoading(false)
-        console.log('logged in successfully')
+        if (result.status) {
+          console.log('login successfully')
+          router.push('/dashboard')
+        }
+        else console.log('error')
       } catch (error) {
-        setError('Invalid username or password')
+        setError('Invalid dni or password')
         setIsLoading(false)
-        setDni('')
-        setPassword('')
+        setData('')
       }
       return
     }
-    // Register
-    console.log('Register')
     try {
-      await userRegister({ dni, name, password })
+      const result = await userRegister(data)
       setIsLoading(false)
-      console.log('register successfully')
+      if (result.status) console.log('register successfully')
+      else console.log('error')
     } catch (error) {
       setError('Invalid data')
       setIsLoading(false)
-      setDni('')
-      setName('')
-      setPassword('')
+      setData('')
     }
   }
 
@@ -90,118 +106,78 @@ const LoginForm = () => {
         {!registerForm ? 'Login' : 'Register'}
       </Heading>
       <Box w="100%" borderWidth={1} borderRadius={8} boxShadow="lg" p="4">
-        <form onSubmit={handleSubmit}>
-          {error && <ErrorMessage message={error} />}
-          {!registerForm ? (
-            <Box>
-              <FormControl isRequired>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {/* {error && <ErrorMessage message={error} />} */}
+          <Box>
+            <FormControl isRequired>
+              <Box mb="5">
+                <FormLabel htmlFor="dni">DNI</FormLabel>
+                <Input
+                  id="dni"
+                  type="number"
+                  placeholder="68234665"
+                  {...register('dni', { required: 'Please enter your dni' })}
+                />
+                {errors.dni && <p>{errors.dni.message}</p>}
+              </Box>
+              {registerForm ? (
                 <Box mb="5">
-                  <FormLabel htmlFor="dni">DNI</FormLabel>
-                  <Input
-                    id="dni"
-                    type="number"
-                    placeholder="68234665"
-                    onChange={(event) => {
-                      setDni(event.currentTarget.value)
-                      setError('')
-                    }}
-                  />
-                </Box>
-                <Box mb="5">
-                  <FormLabel htmlFor="password">Password</FormLabel>
-                  <Input
-                    id="password"
-                    type="password"
-                    placeholder="*******"
-                    onChange={(event) => {
-                      setPassword(event.currentTarget.value)
-                      setError('')
-                    }}
-                  />
-                </Box>
-              </FormControl>
-              <Button
-                type="submit"
-                colorScheme="teal"
-                variantcolor="teal"
-                variant="outline"
-                width="full"
-                mt={4}
-              >
-                {isLoading ? (
-                  <CircularProgress isIndeterminate size="24px" color="teal" />
-                ) : (
-                  'Sign In'
-                )}
-              </Button>
-              <Text align="center" fontSize="sm" mt="3">
-                Don't have an account?{' '}
-                <Link color="teal" onClick={changeForm}>
-                  Register
-                </Link>
-              </Text>
-            </Box>
-          ) : (
-            <Box>
-              <FormControl isRequired>
-                <Box mb="5">
-                  <FormLabel htmlFor="dni">DNI</FormLabel>
-                  <Input
-                    id="dni"
-                    type="number"
-                    placeholder="68234665"
-                    onChange={(event) => {
-                      setDni(event.currentTarget.value)
-                      setError('')
-                    }}
-                  />
-                </Box>
-                <Box mb="5">
-                  <FormLabel htmlFor="name">Nombre</FormLabel>
+                  <FormLabel htmlFor="name">Name</FormLabel>
                   <Input
                     id="name"
                     type="text"
-                    placeholder="Jhon Doe"
-                    onChange={(event) => {
-                      setName(event.currentTarget.value)
-                      setError('')
-                    }}
+                    placeholder="Jhonn Doe"
+                    {...register('name')}
                   />
+                  {errors.name && <p>{errors.name.message}</p>}
                 </Box>
-                <Box mb="5">
-                  <FormLabel htmlFor="password">Password</FormLabel>
-                  <Input
-                    id="password"
-                    type="password"
-                    placeholder="*******"
-                    onChange={(event) => {
-                      setPassword(event.currentTarget.value)
-                      setError('')
-                    }}
-                  />
-                </Box>
-              </FormControl>
-              <Button
-                type="submit"
-                colorScheme="teal"
-                variantcolor="teal"
-                variant="outline"
-                width="full"
-                mt={4}
-              >
-                {isLoading ? (
-                  <CircularProgress isIndeterminate size="24px" color="teal" />
-                ) : (
-                  'Register'
-                )}
-              </Button>
-              <Text align="center" fontSize="sm" mt="3">
-                Have an account?{' '}
-                <Link color="teal" onClick={changeForm}>
-                  Login
-                </Link>
-              </Text>
-            </Box>
+              ) : (
+                ''
+              )}
+              <Box mb="5">
+                <FormLabel htmlFor="password">Password</FormLabel>
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="*******"
+                  {...register('password', {
+                    required: 'Please enter your name'
+                  })}
+                />
+                {errors.password && <p>{errors.password.message}</p>}
+              </Box>
+            </FormControl>
+          </Box>
+          <Button
+            type="submit"
+            colorScheme="teal"
+            variantcolor="teal"
+            variant="outline"
+            width="full"
+            mt={4}
+          >
+            {isLoading ? (
+              <CircularProgress isIndeterminate size="24px" color="teal" />
+            ) : !registerForm ? (
+              'Sign In'
+            ) : (
+              'Sign Up'
+            )}
+          </Button>
+          {!registerForm ? (
+            <Text align="center" fontSize="sm" mt="3">
+              Don't have an account?{' '}
+              <Link color="teal" onClick={changeForm}>
+                Register
+              </Link>
+            </Text>
+          ) : (
+            <Text align="center" fontSize="sm" mt="3">
+              Have an account?{' '}
+              <Link color="teal" onClick={changeForm}>
+                Login
+              </Link>
+            </Text>
           )}
         </form>
       </Box>
