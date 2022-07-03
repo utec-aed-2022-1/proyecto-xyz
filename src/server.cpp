f64a6326bc8c8e4edcf1e57d961709ac831a74b0
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2022-07-03 17:50:10 -0500

    fix(server): CORS fix

--- src/server.cpp
+++ src/server.cpp
@@ -23,29 +23,27 @@ auto main() -> int {
 
   std::cerr << json{bank.getUsers()} << "\n";
 
-  svr.set_pre_routing_handler([](const auto&  /*req*/, auto& res) {
+  svr.set_pre_routing_handler([](const auto& req, auto& res) {
     res.set_header("Access-Control-Allow-Headers", "*");
     res.set_header("Access-Control-Allow-Origin", "*");
-    return Server::HandlerResponse::Handled;
+    if (req.method == "OPTIONS") {
+      return Server::HandlerResponse::Handled;
+    }
+    return Server::HandlerResponse::Unhandled;
   });
 
-  svr.Get("/users", [&bank](const Request& /*req*/, Response& res) {
-    res.set_header("Access-Control-Allow-Origin", "*");
 
+  svr.Get("/users", [&bank](const Request& /*req*/, Response& res) {
     res.set_content(json{bank.getUsers()}.dump(), "application/json");
   });
 
   svr.Get(R"(/user/(\d+))", [&](const Request& req, Response& res) {
-    res.set_header("Access-Control-Allow-Origin", "*");
-
     std::string const& id = req.matches[1];
     res.set_content(json{bank.getUser(id)}.dump(), "application/json");
   });
 
   svr.Post("/user", [&](const Request& /*req*/, Response& res,
                         const ContentReader& content_reader) {
-    res.set_header("Access-Control-Allow-Origin", "*");
-
     content_reader([&](const char* data, size_t /*data_length*/) {
       bank.addUser(json::parse(data).get<User>());
       return true;
@@ -53,15 +51,11 @@ auto main() -> int {
   });
 
   svr.Get("/operations", [&bank](const Request& /*req*/, Response& res) {
-    res.set_header("Access-Control-Allow-Origin", "*");
-
     res.set_content(json(bank.getOperations()).dump(), "application/json");
   });
 
-  svr.Post("/operation", [&](const Request& /*req*/, Response& res,
-                             const ContentReader& content_reader) {
-    res.set_header("Access-Control-Allow-Origin", "*");
-
+  svr.Post("/operations", [&](const Request& /*req*/, Response& res,
+                              const ContentReader& content_reader) {
     content_reader([&](const char* data, size_t /*data_length*/) {
       bank.pushOperation(json::parse(data).get<BankOperation>());
       return true;
